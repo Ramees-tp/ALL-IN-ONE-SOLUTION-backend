575279aa655e8dfcd34d12cf1cb7851ccbb67146
ramees <rameestp001.com>
2024-02-08 20:14:46 +0530

    completed signup and login works

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,59 @@
 const userDetails = require("../models/userDetails");
+const bcrypt = require("bcrypt");
 
 let obj = {
-    signUp: async (req, res) => {
-      const { username, email, password } = req.body;
-      console.log(req.body);
-    
+  signUp: async (req, res) => {
+    const { username, email, password } = req.body;
+    console.log(req.body);
 
-      const newUser = new userDetails({
-        username,
-        email,
-        password,
-      });
-      await newUser.save();
+    try {
+      const existUser = await userDetails.findOne({ email });
+      if (!existUser) {
+        const hashedPass = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+        const newUser = new userDetails({
+          username,
+          email,
+          password: hashedPass,
+        });
+        await newUser.save();
+        return res
+          .status(200)
+          .json({ message: "User registered successfully" });
+      } else {
+        return res
+          .status(400)
+          .json({ message: "User with this email already exists" });
+      }
+    } catch (error) {
+      console.error("Error during signUp:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
+  login: async (req, res) => {
+    const {username, password} = req.body;
+    console.log(req.body);
+
+    try {
+      const existUser = await userDetails.findOne({ username });
+      if (existUser) {
+        const checkPass = bcrypt.compareSync(password, existUser.password);
+        if (checkPass) {
+          return res
+            .status(200)
+            .json({ message: "User logged in successfully" });
+        }else{
+          return res
+          .status(400)
+          .json({ message: "invalid password, try again" }); 
+        }
+      } else {
+        return res
+          .status(400)
+          .json({ message: "User with this Username don't exists" });
+      }
+    } catch (err) {}
   },
 };
 
 module.exports = obj;
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { signUp } = require("../controllers/userController");
+const { signUp, login } = require("../controllers/userController");
 
 router.post("/signUp", signUp);
+router.post("/login",login)
 
 module.exports = router;
