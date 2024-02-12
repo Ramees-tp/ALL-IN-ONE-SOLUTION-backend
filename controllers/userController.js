95629bb9ac75d37b50b7d0a5062f5ea18408c79d
ramees <rameestp001.com>
2024-02-12 20:33:25 +0530

    OTP for reset password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,16 @@
 const userDetails = require("../models/userDetails");
+const userOTP = require("../models/userOTP");
 const bcrypt = require("bcrypt");
+const nodeMailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodeMailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
 
 let obj = {
   signUp: async (req, res) => {
@@ -30,7 +41,7 @@ let obj = {
     }
   },
   login: async (req, res) => {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
     console.log(req.body);
 
     try {
@@ -41,10 +52,10 @@ let obj = {
           return res
             .status(200)
             .json({ message: "User logged in successfully" });
-        }else{
+        } else {
           return res
-          .status(400)
-          .json({ message: "invalid password, try again" }); 
+            .status(400)
+            .json({ message: "invalid password, try again" });
         }
       } else {
         return res
@@ -53,7 +64,83 @@ let obj = {
       }
     } catch (err) {}
   },
+  userOTP: async (req, res) => {
+    const { email } = req.body;
+    try {
+      const existUser = await userDetails.findOne({ email });
+      if (existUser) {
+        const OTP = Math.floor(100000 + Math.random() * 900000);
+        const existMail = await userOTP.findOne({ email });
+        if (existMail) {
+          const updateData = await userOTP.findByIdAndUpdate(
+            { _id: existMail._id },
+            { OTP: OTP },
+            { new: true }
+          );
+          await updateData.save();
+
+          const mail = {
+            from: process.env.EMAIL,
+            to: email,
+            subject: "sending OTP for authentication",
+            text: `OTP:${OTP}`,
+          };
+          transporter.sendMail(mail, (error, info) => {
+            if (error) {
+              res.status(400).json({ error: "email not send" });
+            } else {
+              console.log("email send", info.response);
+              res.status(200).json({ message: "success" });
+            }
+          });
+        } else {
+          const saveNew = new userOTP({
+            email,
+            OTP: OTP,
+          });
+          await saveNew.save();
+
+          const mail = {
+            from: process.env.EMAIL,
+            to: email,
+            subject: "sending email for authentication",
+            text: `OTP:${OTP}`,
+          };
+          transporter.sendMail(mail, (error, info) => {
+            if (error) {
+              res.status(400).json({ error: "email not send" });
+            } else {
+              console.log("email send", info.response);
+              res.status(200).json({ message: "success" });
+            }
+          });
+        }
+      } else {
+        res.status(400).json({ error: "user with this email not exist" });
+      }
+    } catch (err) {
+      res.status(400).json({ error: "ivalid details", err });
+    }
+  },
+  verifyOTP: async (req, res) => {
+    const { verificationId, otpValues } = req.body;
+    const otp = parseInt(otpValues.join(""), 10);
+    try {
+      const otpPass = await userOTP.findOne({ OTP: otp });
+
+      if (otpPass) {
+        await userOTP.findOneAndUpdate({ OTP: otp },{$unset:{OTP:""}});
+        return res.status(200).json({ message: "Correct OTP" });
+      } else {
+        return res.status(400).json({ message: "Incorrect otp" });
+      }
+    } catch (err) {
+      res.status(500).json({ error: "Invalid OTP details", err });
+    }
+  },
+  resetPass:async(req,res)=>{
+     const{password}=req.body
+  }
 };
 
 module.exports = obj;
-
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { signUp, login } = require("../controllers/userController");
+const { signUp, login, userOTP, verifyOTP, resetPass } = require("../controllers/userController");
+
 
 router.post("/signUp", signUp);
 router.post("/login",login)
+router.post("/userOTP",userOTP)
+router.post("/verifyOTP",verifyOTP)
+router.post("/resetPass",resetPass)
 
 module.exports = router;
